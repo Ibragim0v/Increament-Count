91072d7f4b158f8f04e808dc03ee4ddfa50f4268
Ibragim0v <ibragimov.amirshoh@mail.ru>
2022-04-12 14:57:00 +0500

    increamnet

---
+++ js/script.js
@@ -0,0 +1,11 @@
+let addBtn = document.querySelector('#add');
+let subBtn = document.querySelector('#sub');
+let qty = document.querySelector('#qtyBox');
+
+addBtn.addEventListener("click", () => {
+    qty.value = parseInt(qty.value) + 1
+})
+
+subBtn.addEventListener("click", () => {
+    qty.value = parseInt(qty.value) - 1
+})
